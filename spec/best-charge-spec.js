ce17130eb732db9b35c484fc876aa6e2dc7aa2ed
Cisnoc Xie <502021156@qq.com>
2018-07-08 23:49:15 +0800

    pass getBuyItemsList(buyItemsIdNum, allItems) and countPromotionPrice(buyItemsList, promotions)

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -1,3 +1,10 @@
+let best_charge = require('../src/best-charge');
+let bestCharge = best_charge.bestCharge;
+let getBuyItemsList = best_charge.getBuyItemsList;
+let countPromotionPrice = best_charge.countPromotionPrice;
+let loadAllItems = require('../src/items');
+let loadPromotions = require('../src/promotions');
+
 describe('Take out food', function () {
 
   it('should generate best charge when best is 指定菜品半价', function() {
@@ -43,6 +50,56 @@ describe('Take out food', function () {
 总计：24元
 ===================================`.trim()
     expect(summary).toEqual(expected)
+  });
+
+});
+
+describe('Take out food', function () {
+
+  it('getBuyItemsList(buyItemsIdNum, allItems) testing', function() {
+    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    let summary = getBuyItemsList(inputs, loadAllItems());
+    let expected = `[{"id":"ITEM0001","name":"黄焖鸡","price":18,"num":1},{"id":"ITEM0013","name":"肉夹馍","price":6,"num":2},{"id":"ITEM0022","name":"凉皮","price":8,"num":1}]`.trim()
+    expect(JSON.stringify(summary)).toEqual(expected)
+  });
+
+  it('getBuyItemsList(buyItemsIdNum, allItems) testing', function() {
+    let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
+    let summary = getBuyItemsList(inputs, loadAllItems());
+    let expected = `[{"id":"ITEM0013","name":"肉夹馍","price":6,"num":4},{"id":"ITEM0022","name":"凉皮","price":8,"num":1}]`.trim()
+    expect(JSON.stringify(summary)).toEqual(expected)
+  });
+
+  it('getBuyItemsList(buyItemsIdNum, allItems) testing', function() {
+    let inputs = ["ITEM0013 x 4"];
+    let summary = getBuyItemsList(inputs, loadAllItems());
+    let expected = `[{"id":"ITEM0013","name":"肉夹馍","price":6,"num":4}]`.trim()
+    expect(JSON.stringify(summary)).toEqual(expected)
+  });
+
+});
+
+describe('Take out food', function () {
+
+  it('getBuyItemsList(buyItemsIdNum, allItems) testing', function() {
+    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    let summary = countPromotionPrice(getBuyItemsList(inputs, loadAllItems()),loadPromotions());
+    let expected = `{"promotion":"指定菜品半价(黄焖鸡，凉皮)","savedprice":13,"finalprice":25}`.trim()
+    expect(JSON.stringify(summary)).toEqual(expected)
+  });
+
+  it('getBuyItemsList(buyItemsIdNum, allItems) testing', function() {
+    let inputs = ["ITEM0013 x 4", "ITEM0022 x 1"];
+    let summary = countPromotionPrice(getBuyItemsList(inputs, loadAllItems()),loadPromotions());
+    let expected = `{"promotion":"满30减6元","savedprice":6,"finalprice":26}`.trim()
+    expect(JSON.stringify(summary)).toEqual(expected)
+  });
+
+  it('getBuyItemsList(buyItemsIdNum, allItems) testing', function() {
+    let inputs = ["ITEM0013 x 4"];
+    let summary = countPromotionPrice(getBuyItemsList(inputs, loadAllItems()),loadPromotions());
+    let expected = `{"promotion":"","savedprice":0,"finalprice":24}`.trim()
+    expect(JSON.stringify(summary)).toEqual(expected)
   });
 
 });
--- src/best-charge.js
+++ src/best-charge.js
@@ -1,3 +1,51 @@
 function bestCharge(selectedItems) {
+
   return /*TODO*/;
+}
+
+function getBuyItemsList(buyItemsIdNum, allItems) {
+  let buyItemsList = [];
+  let allItemsCopy = JSON.parse(JSON.stringify(allItems));
+  for (let i of buyItemsIdNum) {
+    for (let j of allItemsCopy) {
+      if (i.slice(0,8) === j.id) {
+        j.num = parseInt(i.slice(11));
+        buyItemsList.push(j);
+        break;
+      }
+    }
+  }
+  return buyItemsList;
+}
+
+function countPromotionPrice(buyItemsList, promotions) {
+  let promotionPrice = {promotion:'', savedprice:0, finalprice:0};
+  let second = false;
+  for (let i of buyItemsList) {
+    promotionPrice.finalprice += i.price * i.num;
+    for (let j of promotions[1].items) {
+      if (i.id === j) {
+        second = true;
+        promotionPrice.promotion += i.name + '，';
+        promotionPrice.savedprice += i.price * i.num / 2;
+        break;
+      }
+    }
+  }
+  if (second) {
+    promotionPrice.promotion = promotionPrice.promotion.slice(0,promotionPrice.promotion.length - 1);
+    promotionPrice.promotion = '指定菜品半价(' + promotionPrice.promotion + ')';
+  }
+  if (promotionPrice.finalprice >= 30 && promotionPrice.savedprice < 6) {
+    promotionPrice.promotion = promotions[0].type;
+    promotionPrice.savedprice = 6;
+  }
+  promotionPrice.finalprice -= promotionPrice.savedprice;
+  return promotionPrice;
+}
+
+module.exports = {
+  bestCharge,
+  getBuyItemsList,
+  countPromotionPrice
 }
